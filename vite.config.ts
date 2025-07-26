58bd9417a0c0bc5bdc05fe12cad633ecd59276f0
nlevites <nathanjlevites@gmail.com>
2025-07-25 23:21:07 -0700

    add deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/vor-tutor/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
